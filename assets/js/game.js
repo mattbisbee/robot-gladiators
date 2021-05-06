62c0f654ec589b50fe5500605b694ad218792230
Biznaz <aldhelm7@gmail.com>
2021-05-06 16:51:36 -0700

    shop integer replace string cmds

--- assets/js/game.js
+++ assets/js/game.js
@@ -44,7 +44,6 @@ var fight = function(enemy) {
       // if true, leave the fight by breaking the loop
       break;
     }
-    fightOrSkip();
 
     // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -155,29 +154,22 @@ var endGame = function() {
 var shop = function() {
   // ask the player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 to LEAVE."
   );
-
+shopOptionPrompt = parseInt(shopOptionPrompt);
   // use switch case to carry out action
   switch (shopOptionPrompt) {
-    case "REFILL":
-    case "refill":
+    case 1:
         playerInfo.refillHealth();
         break;
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "LEAVE":
-    case "leave":
+    case 3:
       window.alert("Leaving the store.");
-
-      //do nothing, so function will end
       break;
     default:
       window.alert("you did not pick a valid option. Try again.");
-
-      // call shop() again to force player to pick a valid option
       shop();
       break;
   }
